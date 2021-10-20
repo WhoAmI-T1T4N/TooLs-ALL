eb959ffdc9ca4ebb49e62e91b3f28ebbc4657739
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-10-20 19:38:52 +0700

    Update and rename TooLs-IDN.sh to TooLs-T3RM0X.sh

--- TooLs-T3RM0X.sh
+++ TooLs-T3RM0X.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 
-figlet Tools
-figlet Termux
+figlet Tools | lolcat
+figlet Termux | lolcat
 echo "==========================================="
 echo "Author : Mr.T1T4N"
 echo "Team : Indonesia Cyber | Linux Cyber"
 echo "Youtube : T1T4N TZY"
 echo "==========================================="
+echo
 echo "==========================================="
 echo "1). Tools Spam"
-echo "2). Tools Hack CCTV"
-echo "3). Tools Script DEFACE"
 echo "99). Keluar"
 echo "==========================================="
 read -p "PilihLah Sesuka Kalian :" who
+
 if [ $who = 1 ]
 then
    git clone https://github.com/WhoAmI-T1T4N/SpamV2
-     cd SpamV2
+    cd SpamV2
      bash WhoAmI.sh
-if [ $who = 2 ]
-then
-   git clone https://github.com/MrVirusSpm-07/cchek
-     cd cchek
-     bash cchek.sh
-if [ $who = 3 ]
-then
-   git clone https://github.com/4LL13199/LITESCRIPT
-     cd LITESCRIPT
-     python2 LITESCRIPT.py
-if [ $who = 99 ]
-then
-figlet Thanks | lolcat
-figlet You | lolcat
+fi
